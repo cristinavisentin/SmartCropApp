adf7b51bc7a7f3c41d745d8a6eaaa1e9527928ab
Gabriele <tomaigabriele@gmail.com>
2025-01-18 16:35:27 +0100

    feat: first implementation of cookies (only for develop time)

--- app/sign_in.py
+++ app/sign_in.py
@@ -1,12 +1,19 @@
 import streamlit as st
 from db_utils import check_user_credentials
+from streamlit_cookies_controller import CookieController
+import time
+
+controller = CookieController(key='AgricultureAuth')
+
 
 def sign_in_page():
+    show_cookies()
     st.title("Sign in")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     if st.button("Sign In"):
         if check_user_credentials(username, password):
+            save_cookies(username, password)
             st.success(f"Welcome, {username}!")
             st.session_state["page"] = "cultivation"
             st.rerun()
@@ -15,3 +22,18 @@ def sign_in_page():
     if st.button("Don't have an account? Register"):
         st.session_state["page"] = "sign_up"
         st.rerun()
+
+
+def save_cookies(username, password):
+    controller.set("AgricultureAuth_username", username)
+    controller.set("AgricultureAuth_password", password)
+    cookies = controller.getAll()
+    time.sleep(1)  # Aggiunge un leggero ritardo
+    st.write("Cookie aggiornati:", cookies)
+
+def show_cookies():
+    cookies = controller.getAll()
+    time.sleep(1)  # Aggiunge un leggero ritardo
+    st.write("Cookie inizialmente trovati:", cookies)
+
+
--- app/sign_up.py
+++ app/sign_up.py
@@ -1,12 +1,16 @@
 import streamlit as st
 from db_utils import create_user
+from sign_in import save_cookies
+from sign_in import show_cookies
 
 def sign_up_page():
+    show_cookies()
     st.title("Sign Up")
     username = st.text_input("Choose a Username")
     password = st.text_input("Choose a Password", type="password")
     if st.button("Sign Up"):
         if create_user(username, password):
+            save_cookies(username, password)
             st.success("Successful registration! Back to login page")
             st.session_state["page"] = "login"
             st.rerun()
