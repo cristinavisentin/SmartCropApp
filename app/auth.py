48fcfab4facbca8c675e4fe219c5204ef5b7cd36
Gabriele <tomaigabriele@gmail.com>
2025-01-20 20:02:01 +0100

    feat: checkbox added: "Do you want to stay logged in?" and clean code

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import time
 from db_utils import check_user_credentials, create_user
-from cookie_handler import show_cookies, save_cookies
+from cookie_handler import save_cookies
 
 def sign_in_page():
-    print("cookie founded in sign_in_page: ")
-    show_cookies()
     st.title("Sign in")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
+    stay_logged = st.checkbox("Do you want to stay logged in?")
     if st.button("Sign In"):
         if check_user_credentials(username, password):
-            save_cookies(username)
+            if stay_logged:
+                save_cookies(username)
             st.session_state["user_id"] = username
             st.success(f"Welcome, {username}!")
             time.sleep(0.5)
@@ -25,14 +25,11 @@ def sign_in_page():
         st.rerun()
 
 def sign_up_page():
-    print("cookie trovati in sign_up page: ")
-    show_cookies()
     st.title("Sign Up")
     username = st.text_input("Choose a Username")
     password = st.text_input("Choose a Password", type="password")
     if st.button("Sign Up"):
         if create_user(username, password):
-            save_cookies(username)
             st.success("Successful registration! Back to login page")
             st.session_state["page"] = "sign_in"
             st.rerun()
