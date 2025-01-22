a39e430e7cffd1d1ba1c8a7b3d73d1554771f4aa
Gabriele <tomaigabriele@gmail.com>
2025-01-22 11:25:48 +0100

    feat: added exception handling in case db is not initialized

--- app/auth.py
+++ app/auth.py
@@ -17,19 +17,21 @@ def sign_in_page():
     password = st.text_input("Password", type="password")
     remember = st.checkbox("Do you want to stay logged in?")
     if st.button("Sign In"):
-        result = db_utils.check_and_mem_user_credentials(username, password)
-        if result:
-            if remember:
-                save_persistent_session_auth_token(username)
-            st.session_state["authenticated"] = True
-            st.session_state["username"] = username
-
-            st.success(f"Welcome, {username}!")
-            time.sleep(0.5)
-            st.session_state["page"] = "crop_application"
-            st.rerun()
+        result, error_message = db_utils.check_user_credentials(username, password)
+        if error_message:
+            st.error(f"There is a problem with the database. We apologize for the inconvenience, please try again later")
         else:
-            st.error("Invalid credentials. Retry")
+            if result:
+                if remember:
+                    save_persistent_session_auth_token(username)
+                st.session_state["authenticated"] = True
+                st.session_state["username"] = username
+                st.success(f"Welcome, {username}!")
+                time.sleep(0.5)
+                st.session_state["page"] = "crop_application"
+                st.rerun()
+            else:
+                st.error("Invalid credentials. Retry")
     if st.button("Don't have an account? Register"):
         st.session_state["page"] = "sign_up"
         time.sleep(0.5)
@@ -40,12 +42,16 @@ def sign_up_page():
     username = st.text_input("Choose a Username")
     password = st.text_input("Choose a Password", type="password")
     if st.button("Sign Up"):
-        if db_utils.create_user(username, password):
-            st.success("Successful registration! Back to login page")
-            st.session_state["page"] = "sign_in"
-            st.rerun()
+        result, error_message = db_utils.create_user(username, password)
+        if error_message:
+            st.error(f"There is a problem with the database. We apologize for the inconvenience, please try again later")
         else:
-            st.error("Error: username already exists. Choose another username")
+            if result:
+                st.success("Successful registration! Back to login page")
+                st.session_state["page"] = "sign_in"
+                st.rerun()
+            else:
+                st.error("Error: username already exists. Choose another username")
     if st.button("Already have an account? Go to Login"):
         st.session_state["page"] = "sign_in"
         time.sleep(0.5)
--- app/db_utils.py
+++ app/db_utils.py
@@ -12,44 +12,53 @@ def create_connection(db_file):
     return sqlite3.connect(db_file)
 
 def create_user(username, password):
-    conn = create_connection("users.db")
-    cursor = conn.cursor()
-    salt = os.urandom(16)
     try:
+        conn = create_connection("users.db")
+        cursor = conn.cursor()
+        salt = os.urandom(16)
         cursor.execute("INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)", (username, hash_password(password, salt), salt.hex()))
         conn.commit()
         conn.close()
-        return True
+        return True, None
     except sqlite3.IntegrityError:
+        print("User already exists")
         return False
+    except sqlite3.OperationalError as e:
+        print(f"Database error: {e}")
+        return False, str(e)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return False, str(e)
 
-def check_and_mem_user_credentials(username, password):
-    conn = create_connection("users.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT username, hashed_password, salt FROM users WHERE username=?", (username,))
-    user_data = cursor.fetchone()
-    conn.close()
-
-    if user_data:
-        user_name, hashed_password, salt = user_data
-        retrieved_salt = bytes.fromhex(salt)
-        if user_data[1] == hash_password(password, retrieved_salt):
-            return True
-    return False
+def check_user_credentials(username, password):
+    try:
+        conn = create_connection("users.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT username, hashed_password, salt FROM users WHERE username=?", (username,))
+        user_data = cursor.fetchone()
+        conn.close()
+        if user_data:
+            user_name, hashed_password, salt = user_data
+            retrieved_salt = bytes.fromhex(salt)
+            if hashed_password == hash_password(password, retrieved_salt):
+                return True, None
+    except (sqlite3.DatabaseError, TypeError, ValueError) as e:
+        print(f"An error occurred while processing the request: {e}")
+        print("You probably need to initializate the database")
+        return False, str(e)
+    return False, None
 
 
 def check_username_in_db(username):
-    conn = create_connection("users.db")
-    cursor = conn.cursor()
-    # Stampa i dati della tabella
-    cursor.execute("SELECT * FROM users")  # Seleziona tutti i record
-    rows = cursor.fetchall()  # Ottieni tutte le righe
-    print("Contenuto della tabella 'users' in check_username_in_db:")
-    for row in rows:
-        print(row)
-    print("\n")
-
     try:
+        conn = create_connection("users.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        print("'user' table content in check_username_in_db:")
+        for row in rows:
+            print(row)
+        print("\n")
         cursor.execute("SELECT 1 FROM users WHERE username = ?", (username,))
         result = cursor.fetchone()
 
