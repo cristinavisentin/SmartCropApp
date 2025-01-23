0d1048a2e66cacb8286ee50d74d3c39bbef63580
Gabriele <tomaigabriele@gmail.com>
2025-01-23 12:26:00 +0100

    feat: first implementation of model in get_result.py

--- app/prediction_result.py
+++ app/prediction_result.py
@@ -1,4 +1,37 @@
 import streamlit as st
+import pickle
+
+import os
+base_dir = os.path.dirname(os.path.abspath(__file__))
+encoder_path = os.path.join(base_dir, '../artifacts/encoder.pkl')
+
+with open(os.path.join(base_dir, '../artifacts/encoder.pkl'), 'rb') as file:
+    encoder = pickle.load(file)
+with open(os.path.join(base_dir, '../artifacts/scaler.pkl'), 'rb') as file:
+    scaler = pickle.load(file)
+with open(os.path.join(base_dir, '../artifacts/model.pkl'), 'rb') as file:
+    model = pickle.load(file)
+
+
+def get_prediction(plant, country, avg_temperature, avg_rainfall):
+    encoded = encoder.transform([['Wheat', 'Italy']])
+    other_vals = [[2025, 0.3, 4.5]]
+
+    other_vals[0].extend(encoded[0])
+    to_predict = scaler.transform(other_vals)
+
+    print("modello ottenuto in get_prediction(): ", model.predict(to_predict))
+    return 1
+
+def display_result(plant, country, avg_temperature, avg_rainfall):
+    print("plant: ", plant, "country: ", country)
+    st.write("plant: ", plant, "country: ", country)
+    print("avg_rainfall: ", avg_rainfall, "mm, avg_temperature: ", avg_temperature, "C")
+    st.write("avg_rainfall: ", avg_rainfall, "mm, avg_temperature: ", avg_temperature, "C")
+    predit = get_prediction(plant, country, avg_temperature, avg_rainfall)
+    print("predit: ", predit, "etti/ettaro")
+    st.write("predit: ", predit, "etti/ettaro")
+
 
 def prediction_result_page():
     st.title("Prediction Result")
@@ -24,12 +57,7 @@ def prediction_result_page():
             avg_rainfall = avg_rainfall[0]
 
             print("FIRST UNIQUE PREDICTION")
-            print("plant: ", plant, "country: ", country)
-            st.write("FIRST UNIQUE PREDICTION")
-            st.write("plant: ", plant, "country: ", country)
-            print("avg_rainfall: ", avg_rainfall, "mm, avg_temperature: ", avg_temperature, "C")
-            st.write("avg_rainfall: ", avg_rainfall, "mm, avg_temperature: ", avg_temperature, "C")
-
+            display_result(plant, country, avg_temperature, avg_rainfall)
 
 
         else:
@@ -41,35 +69,25 @@ def prediction_result_page():
                 first_country = country[0]
                 first_avg_temperature = avg_temperature[0]
                 first_avg_rainfall = avg_rainfall[0]
-                print("FIRST PREDICTION")
-                print("plant: ", first_plant, "country: ", first_country)
-                st.write("FIRST PREDICTION")
-                st.write("plant: ", first_plant, "country: ", first_country)
-                print("avg_rainfall: ", first_avg_rainfall, "mm, avg_temperature: ", first_avg_temperature, "C")
-                st.write("avg_rainfall: ", first_avg_rainfall, "mm, avg_temperature: ", first_avg_temperature, "C")
+
+                display_result(first_plant, first_country, first_avg_temperature, first_avg_rainfall)
 
                 second_plant = plants[1]
                 second_country = country[1]
                 second_avg_temperature = avg_temperature[1]
                 second_avg_rainfall = avg_rainfall[1]
-                print("SECOND PREDICTION")
-                st.write("SECOND PREDICTION")
-                print("plant: ", second_plant, "country: ", second_country)
-                st.write("plant: ", second_plant, "country: ", second_country)
-                print("avg_rainfall: ", second_avg_rainfall, "mm, avg_temperature: ", second_avg_temperature, "C")
-                st.write("avg_rainfall: ", second_avg_rainfall, "mm, avg_temperature: ", second_avg_temperature, "C")
+
+                display_result(second_plant, second_country, second_avg_temperature, second_avg_rainfall)
 
                 third_plant = plants[2]
                 third_country = country[2]
                 third_avg_temperature = avg_temperature[2]
                 third_avg_rainfall = avg_rainfall[2]
-                print("THIRD PREDICTION")
-                st.write("THIRD PREDICTION")
-                print("plant: ", third_plant, "country: ", third_country)
-                st.write("plant: ", third_plant, "country: ", third_country)
-                print("avg_rainfall: ", third_avg_rainfall, "mm, avg_temperature: ", third_avg_temperature, "C")
-                st.write("avg_rainfall: ", third_avg_rainfall, "mm, avg_temperature: ", third_avg_temperature, "C")
 
+                display_result(third_plant, third_country, third_avg_temperature, third_avg_rainfall)
 
     else:
         st.write("No data found in session state.")
+
+
+
