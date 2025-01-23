ef2fd7b570eba12c298f29f7fea3663ece0be515
Gabriele <tomaigabriele@gmail.com>
2025-01-23 17:02:45 +0100

    feat: added add_prediction_to_db() to prediction_result.py and refactor file

--- app/prediction_result.py
+++ app/prediction_result.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 
 import os
+from db_utils import add_prediction_to_db
+
 base_dir = os.path.dirname(os.path.abspath(__file__))
 encoder_path = os.path.join(base_dir, '../artifacts/encoder.pkl')
 
@@ -13,53 +15,47 @@ with open(os.path.join(base_dir, '../artifacts/model.pkl'), 'rb') as file:
     model = pickle.load(file)
 
 
-def get_prediction(plant, country, avg_temperature, avg_rainfall):
-    encoded = encoder.transform([['Wheat', 'Italy']])
-    other_vals = [[2025, 0.3, 4.5]]
+def get_prediction(plant, country, avg_temperature, avg_rainfall, hectares):
+    print("VALUED USED IN THIS PREDICTION:", plant, country, avg_temperature, avg_rainfall)
+    encoded = encoder.transform([[plant, country]])
+    other_vals = [[2025, avg_rainfall, avg_temperature]]
 
     other_vals[0].extend(encoded[0])
     to_predict = scaler.transform(other_vals)
 
-    acres = st.session_state["acres_to_cultivate"]
-    print("acres received: ", acres)
-    print("model in get_prediction(): ", model.predict(to_predict))
-    return 1
+    print("hectares received: ", hectares)
+    prediction = model.predict(to_predict)
+    print("model in get_prediction(): ", prediction)
+    return prediction
 
-def display_result(plant, country, avg_temperature, avg_rainfall):
-    print("plant: ", plant, "country: ", country)
+def display_result_and_add_to_db(plant, country, avg_temperature, avg_rainfall, hectares):
     st.write("plant: ", plant, "country: ", country)
-    print("avg_rainfall: ", avg_rainfall, "mm, avg_temperature: ", avg_temperature, "C")
     st.write("avg_rainfall: ", avg_rainfall, "mm, avg_temperature: ", avg_temperature, "C")
-    predit = get_prediction(plant, country, avg_temperature, avg_rainfall)
-    print("predit: ", predit, "ounces/hectare")
-    st.write("predit: ", predit, "ounces/hectare")
+    prediction = get_prediction(plant, country, avg_temperature, avg_rainfall, hectares)
+    add_prediction_to_db(st.session_state["username"], plant, country, hectares, prediction)
+    st.write("prediction: ", prediction, "ounces/hectare")
 
 
 def prediction_result_page():
     st.title("Prediction Result")
-    st.write("For now, these are the data that the user entered, and the data that were retrieved through external weather services")
-
     if "plant" in st.session_state and "country" in st.session_state:
         plants = st.session_state["plant"]
         country = st.session_state["country"]
         avg_temperature = st.session_state["avg_temperature"]
         avg_rainfall = st.session_state["avg_rainfall"]
-
-        print("plants: ", plants)
-
-        if len(plants) == 1 and len(country) != 1:
+        hectares = st.session_state["acres_to_cultivate"]
+        if (len(plants) == 1 and len(country) != 1) or (len(plants) != 1 and len(country) == 1):
             print("Received bad data 1")
-        elif len(plants) != 1 and len(country) == 1:
-            print("Received bad data 2")
-        elif len(plants) == 1 and len(country) == 1:
+        elif len(plants) == 1 and len(country) == 1 and len(hectares) == 1:
             print("One prediction requested")
             plant = plants[0]
             country = country[0]
             avg_temperature = avg_temperature[0]
             avg_rainfall = avg_rainfall[0]
+            hectares = hectares[0]
 
             print("FIRST UNIQUE PREDICTION")
-            display_result(plant, country, avg_temperature, avg_rainfall)
+            display_result_and_add_to_db(plant, country, avg_temperature, avg_rainfall, hectares)
 
 
         else:
@@ -67,26 +63,9 @@ def prediction_result_page():
                 print("Received bad data 3")
             else:
                 print("Three predictions requested")
-                first_plant = plants[0]
-                first_country = country[0]
-                first_avg_temperature = avg_temperature[0]
-                first_avg_rainfall = avg_rainfall[0]
-
-                display_result(first_plant, first_country, first_avg_temperature, first_avg_rainfall)
-
-                second_plant = plants[1]
-                second_country = country[1]
-                second_avg_temperature = avg_temperature[1]
-                second_avg_rainfall = avg_rainfall[1]
-
-                display_result(second_plant, second_country, second_avg_temperature, second_avg_rainfall)
-
-                third_plant = plants[2]
-                third_country = country[2]
-                third_avg_temperature = avg_temperature[2]
-                third_avg_rainfall = avg_rainfall[2]
-
-                display_result(third_plant, third_country, third_avg_temperature, third_avg_rainfall)
+                display_result_and_add_to_db(plants[0], country[0], avg_temperature[0], avg_rainfall[0], hectares[0])
+                display_result_and_add_to_db(plants[1], country[1], avg_temperature[1], avg_rainfall[1], hectares[1])
+                display_result_and_add_to_db(plants[2], country[2], avg_temperature[2], avg_rainfall[2], hectares[2])
 
     else:
         st.write("No data found in session state.")
